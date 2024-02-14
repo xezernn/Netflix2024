f843624f1fecb06ed8e717ba031ae9ecd94e42cb
xezernn <xezernovruz79@gmail.com>
2024-02-15 01:40:14 +0400

    Fairbase.js full function, toaster

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import toast from "react-hot-toast";
 const firebaseConfig = {
-    apiKey: "AIzaSyAry5kuvUY7NuVlYzD1p5idqsOJjqruS_U",
-    authDomain: "netflix-dd305.firebaseapp.com",
-    projectId: "netflix-dd305",
-    storageBucket: "netflix-dd305.appspot.com",
-    messagingSenderId: "851595218010",
-    appId: "1:851595218010:web:f8f505a50d1620941c9c63"
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGIN_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 
@@ -15,10 +15,31 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 export const registr = async (email, password) => {
-    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+    try {
+        const { user } = await createUserWithEmailAndPassword(auth, email, password)
 
-    return user
+        return user
+    } catch (error) {
+        toast.error(error.message);
+    }
 }
 
+export const login = async (email, password) => {
+    try {
+        const { user } = await signInWithEmailAndPassword(auth, email, password)
+        return user
+    } catch (error) {
+        toast.error(error.message);
+    }
+}
+
+export const logout = async () => {
+    try {
+        await signOut(auth)
+        return true
+    } catch (error) {
+        toast.error(error.message);
+    }
+}
 
 export default app
