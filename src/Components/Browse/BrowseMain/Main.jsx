774f094dab4620ced40f74443c2b1d88148a9c63
xezernn <xezernovruz79@gmail.com>
2024-02-21 08:36:10 +0400

    sliders

--- src/Components/Browse/BrowseMain/Main.jsx
+++ src/Components/Browse/BrowseMain/Main.jsx
@@ -4,7 +4,7 @@ import Sliders1 from './Sliders/Sliders1'
 
 function Main() {
     return (
-        <main className='h-screen bg-[#141414] '>
+        <main className='bg-[#141414] '>
             <MainIframe />
             <div className='relative  top-[-8%] xmm:top-[-5%] md:top-[-27%] z-10'>
                 <div className='   '>
@@ -12,6 +12,13 @@ function Main() {
                     <Sliders1 />
                     <Sliders1 />
                     <Sliders1 />
+                    <Sliders1 />
+                    <Sliders1 />
+                    <Sliders1 />
+                    <Sliders1 />
+                    <Sliders1 />
+                    <Sliders1 />
+                    <div className='py-8'>  </div>
                 </div>
             </div>
         </main>
